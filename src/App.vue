1639c9c82473259db8e1a07b879fcc6c26c57598
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-02-15 21:18:14 +0300

    Получение данных с сервера

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
-    <post-list :posts="posts" @remove="removePost" />
+    <h1>Страница с постами</h1>
+    <my-button
+      @click="showDialog"
+      style="margin: 15px 0;"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
+    <post-list :posts="posts" @remove="removePost" v-if="!isPostsLoading" />
+    <div v-else>Идет загрузка...</div>
   </div>
 </template>
   
 <script>
 import PostForm from './components/PostForm'
 import PostList from './components/PostList'
+import axios from 'axios';
 
 export default {
   components: {
@@ -15,21 +26,37 @@ export default {
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: 'JavaScript', body: 'О JS' },
-        { id: 2, title: 'JavaScript 2', body: 'О JS 2' },
-        { id: 3, title: 'JavaScript 3', body: 'О JS 3' },
-      ],
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
     }
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        this.posts = response.data;
+      } catch (e) {
+        alert('Ошибка')
+      } finally {
+        this.isPostsLoading = false;
+      }
     }
-  }
+  },
+  mounted() {
+    this.fetchPosts();
+  },
 }
 </script>
 
